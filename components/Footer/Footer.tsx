a1b80526b8a06eda137dca5994c98cb008038167
Kieran Gillen <kgillen@gmail.com>
2020-04-08 12:31:01 -0400

    Styles footer for mobile

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import { Sans, Box, Flex, Spacer } from "../"
+import { color } from "../../helpers"
+import { Link } from "../Link"
+
+export const Footer = () => {
+  return (
+    <Flex
+      px={2}
+      pb={[1, 0]}
+      style={{ borderTop: `1px solid ${color("black10")}`, width: "100%" }}
+      justifyContent={["flex-start", "center"]}
+    >
+      <Flex
+        flexDirection={["column", "row"]}
+        py={2}
+        style={{ maxWidth: "1200px", flex: 1 }}
+        justifyContent={["flex-start", "space-between"]}
+      >
+        <Link href="/terms-of-service">
+          <Sans size="4" color={color("black50")} pt="0.5" pr="3">
+            Terms of Service
+          </Sans>
+        </Link>
+        <Spacer mb={[1, 0]} />
+        <Link href="/privacy-policy">
+          <Sans size="4" color={color("black50")} pt="0.5" pr="3">
+            Privacy Policy
+          </Sans>
+        </Link>
+        <Spacer mb={[1, 0]} />
+        <Box ml={[0, "auto"]}>
+          <Sans size="4" color={color("black50")} pt="0.5">
+            (c) 2020 Seasons. All Rights Reserved.
+          </Sans>
+        </Box>
+      </Flex>
+    </Flex>
+  )
+}
