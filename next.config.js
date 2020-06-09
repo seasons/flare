fc4dc8e1fe2982cbffb13852de8edc1d37d542b8
Kieran Gillen <kgillen@gmail.com>
2020-06-09 14:50:00 -0400

    Adds ENV to next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = withImages(
   withFonts({
     env: {
       MONSOON_ENDPOINT: process.env.MONSOON_ENDPOINT,
+      NODE_ENV: process.env.NODE_ENV,
+      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     },
   })
 )
