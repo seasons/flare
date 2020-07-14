476adf447439be2b61576b98e40d5ccce70ce2e3
Luc Succes <l2succes@gmail.com>
2020-07-14 14:52:02 -0400

    Update app link

--- components/Button/GetTheApp.tsx
+++ components/Button/GetTheApp.tsx
@@ -1,9 +1,10 @@
-import { color } from "../../helpers"
 import styled from "styled-components"
-import { Button } from "./Button"
-import { Flex, Spacer, Sans } from "../"
-import { AppleSVG } from "../SVGs"
+
+import { Flex, Sans, Spacer } from "../"
+import { color } from "../../helpers"
 import { themeProps } from "../../lib/theme"
+import { AppleSVG } from "../SVGs"
+import { Button } from "./Button"
 
 interface Props {
   block?: boolean
@@ -12,10 +13,7 @@ interface Props {
 export const GetTheAppButton: React.FC<Props> = ({ block }) => {
   return (
     <GetAppWrapper block={block}>
-      <Button
-        width={block ? "100%" : "auto"}
-        onClick={() => window.open("https://testflight.apple.com/join/fpXqUY0v", "_blank")}
-      >
+      <Button width={block ? "100%" : "auto"} onClick={() => window.open("https://szns.co/app", "_blank")}>
         <Flex width="100%" justifyContent="center" flexDirection="row" alignContent="center">
           <AppleSVG width="17px" height="20px" />
           <Spacer mr={1} />
