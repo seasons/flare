43a328d5dcededfd3d2b79150c84ef4c1511ae04
Kieran Gillen <kgillen@gmail.com>
2020-08-06 21:44:10 -0400

    Fixes footer margins

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -14,51 +14,51 @@ export const Footer: React.FC<{ footerBottomPadding?: string | string[] }> = ({ 
       <Row px={[2, 2, 2, 5, 5]}>
         <Col lg="2" xs="9">
           <Sans size="3">Info</Sans>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <Sans size="3" color="black50">
             A members-only rental platform for designer menswear & streetwear.
           </Sans>
-          <Spacer mb={[3, 0]} />
+          <Spacer mb={[3, 3, 3, 0, 0]} />
         </Col>
         <Col lg="2" xs="12" lgOffset={4}>
           <Sans size="3">Misc.</Sans>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <Link href="/privacy-policy">
             <Sans size="3" color="black50">
               Privacy Policy
             </Sans>
           </Link>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <Link href="/terms-of-service">
             <Sans size="3" color={color("black50")} pt="0.5" pr="3">
               Terms of Service
             </Sans>
           </Link>
-          <Spacer mb={[3, 0]} />
+          <Spacer mb={[3, 3, 3, 0, 0]} />
         </Col>
         <Col lg="2" xs="12">
           <Sans size="3">Get in touch</Sans>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <a href="mailto:membership@seasons.nyc?subject=Hello" style={{ textDecoration: "none" }}>
             <Sans size="3" color="black50">
               membership@seasons.nyc
             </Sans>
           </a>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <Sans size="3" color={color("black50")} pt="0.5" pr="3">
             New York, NY
           </Sans>
-          <Spacer mb={[3, 0]} />
+          <Spacer mb={[3, 3, 3, 0, 0]} />
         </Col>
         <Col lg="2" xs="12">
           <Sans size="3">Follow us</Sans>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <a href="https://www.instagram.com/seasons.ny/" target="_blank" style={{ textDecoration: "none" }}>
             <Sans size="3" color={color("black50")} pt="0.5" pr="3">
               Instagram
             </Sans>
           </a>
-          <Spacer mb={[1, 2]} />
+          <Spacer mb={[1, 1, 1, 2, 2]} />
           <a href="https://www.instagram.com/seasons.ny/" target="_blank" style={{ textDecoration: "none" }}>
             <Sans size="3" color={color("black50")} pt="0.5" pr="3">
               Twitter
