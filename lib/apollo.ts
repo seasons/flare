44ed926d8885106e4396aeff1590184f80f5b337
Kieran Gillen <kgillen@gmail.com>
2020-08-25 17:12:06 -0400

    Checks for localStorage

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -5,7 +5,7 @@ import { setContext } from "apollo-link-context"
 // Set up Apollo Client
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token")
+  const token = localStorage?.getItem("token")
 
   // return the headers to the context so httpLink can read them
   const allHeaders = headers || {}
