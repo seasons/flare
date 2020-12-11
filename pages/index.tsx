a72aae89c09e1029727d541b125d21caa672b8e4
Kieran Gillen <kgillen@gmail.com>
2020-12-11 16:33:36 -0500

    Adds collection page

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,6 +57,14 @@ const Home = screenTrack(() => ({
       <ProductRail title="Just added tops" products={data?.justAddedTops} />
       <Spacer mb={10} />
 
+      {!!data?.holiday?.length && (
+        <>
+          <Spacer mb={10} />
+          <ProductRail title="Holiday cozy collection" products={data?.holiday} tag="Holiday" />
+          <Spacer mb={10} />
+        </>
+      )}
+
       <FromCommunity blogPosts={communityPosts} />
 
       {!!data?.justAddedBottoms?.length && (
@@ -111,10 +119,6 @@ const Home = screenTrack(() => ({
       <Spacer mb={10} />
       <FAQ />
       <Spacer mb={15} />
-
-      <Media greaterThan="md">
-        <ServiceableModal />
-      </Media>
     </Layout>
   )
 })
