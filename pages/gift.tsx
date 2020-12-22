c8cbca91e3d987c12a2bb5adec13318e3f26cbec
Luc Succes <l2succes@gmail.com>
2020-12-22 13:42:36 -0500

    Fixes gift page layout

--- pages/gift.tsx
+++ pages/gift.tsx
@@ -1,18 +1,18 @@
-import { Box, Button, Flex, Layout, Spacer, TextInput } from "components"
-import { usePopUpContext } from "components/PopUp/PopUpContext"
+import {
+  Box, Button, Display, Flex, Layout, MaxWidth, Media, Separator, Spacer, TextInput
+} from "components"
+import { Col, Grid, Row } from "components/Grid"
 import { ZIPCODE_SERVICED } from "components/ServiceableModal"
-import { ListCheck } from "components/SVGs/ListCheck"
+import { groupByPlanTier } from "components/SignUp/MembershipPlans"
+import { PlanTier } from "components/SignUp/PlanTier"
 import { initializeApollo } from "lib/apollo/apollo"
 import { executeChargebeeCheckout, initChargebee } from "lib/chargebee"
-import { uniq } from "lodash"
-import { PlanButton } from "mobile/Account/Components/PlanButton"
-import { TabBar } from "mobile/TabBar"
-import { NAVIGATION_QUERY } from "queries/navigationQueries"
 import React, { useEffect, useState } from "react"
 import { Schema, screenTrack, useTracking } from "utils/analytics"
 
 import { gql, useLazyQuery, useQuery } from "@apollo/client"
-import { Sans } from "@seasons/eclipse"
+import { TextField } from "@material-ui/core"
+import { color, Sans } from "@seasons/eclipse"
 
 export const GET_GIFT_PAGE = gql`
   query GetGiftPage($where: PaymentPlanWhereInput) {
@@ -43,15 +43,10 @@ const Gift = screenTrack(() => ({
   path: "/gift",
 }))(() => {
   const { data } = useQuery(GET_GIFT_PAGE)
-  const tracking = useTracking()
   const featuredBrandItems = data?.brands || []
   const plans = data?.paymentPlans
 
-  const [email, setEmail] = useState("")
   const [zipcode, setZipcode] = useState("")
-  const { showPopUp, hidePopUp } = usePopUpContext()
-  const [tiers, setTiers] = useState([])
-  const [currentView, setCurrentView] = useState(0)
   const [selectedPlan, setSelectedPlan] = useState(null)
   const [zipcodeServiced, { data: zipcodeData }] = useLazyQuery(ZIPCODE_SERVICED)
 
@@ -61,157 +56,134 @@ const Gift = screenTrack(() => ({
     initChargebee()
   }, [])
 
-  const tierToReadableText = (tier) => {
-    if (tier === "AllAccess") {
-      return "All Access"
-    } else {
-      return tier
-    }
-  }
-
-  useEffect(() => {
-    // Update the selected plan if you switch tabs
-    const newSelectedPlan =
-      plans?.filter(
-        (plan) => tierToReadableText(plan.tier) === tiers?.[currentView] && plan.itemCount === selectedPlan?.itemCount
-      ) || plans?.filter((plan) => tierToReadableText(plan.tier) === tiers?.[currentView])?.[0]
-    setSelectedPlan(newSelectedPlan?.[0])
-  }, [currentView, setSelectedPlan])
-
-  useEffect(() => {
-    if (plans && plans.length > 0) {
-      setSelectedPlan(plans?.[0])
-      const planTiers = uniq(plans?.map((plan) => tierToReadableText(plan.tier)))
-      setTiers(planTiers)
-    }
-
-    const customerPlan = data?.me?.customer?.membership?.plan
-    const initialPlan = customerPlan ? plans?.find((plan) => plan.id === customerPlan.id) : plans?.[0]
-    const initialTab = customerPlan?.tier === "AllAccess" ? 1 : 0
-
-    setCurrentView(initialTab)
-    setSelectedPlan(initialPlan)
-  }, [plans])
-
   useEffect(() => {
     if (zipcode && zipcode.match(ZIPCODE_REGEX)) {
       zipcodeServiced({
-        variables: { zipcode },
+        variables: { zipCode: zipcode },
       })
     }
   }, [zipcode])
 
-  const descriptionLines = selectedPlan?.description?.split("\n") || []
-  const planColors = ["#000", "#e6b759"]
-  const currentColor = planColors[currentView] || "black"
+  useEffect(() => {
+    if (plans && plans.length > 0) {
+      setSelectedPlan(plans[0])
+    }
+  }, [plans])
+
+  const plansGroupedByTier = groupByPlanTier(plans)
+
+  const onSelectPlan = (plan) => {
+    setSelectedPlan(plan)
+  }
 
   return (
     <Layout fixedNav brandItems={featuredBrandItems}>
-      <Box my={2}>
-        <TextInput
-          autoCapitalize="words"
-          currentValue={email}
-          headerText="Recipient Email"
-          onChangeText={(_, val) => setEmail(val)}
-        />
-      </Box>
-      <Box my={2}>
-        <Sans color="black50" size="3">
-          Please enter your recipient's zipcode to see what plans are available for them
-        </Sans>
-        <TextInput
-          autoCapitalize="words"
-          currentValue={zipcode}
-          headerText="Recipient Zip code"
-          onChangeText={(_, val) => setZipcode(val)}
-        />
-      </Box>
-      <Flex style={{ flex: 1, overflow: "auto" }}>
-        <Box>
-          <Spacer mb={15} />
-          <Box px={2}>
-            <Sans color="black100" size="6">
-              Choose your a Gift Membership
-            </Sans>
-            <Spacer mb={1} />
-            <Sans color="black50" size="3">
-              Here's what will be included in their plan:
-            </Sans>
-            <Spacer mb={1} />
+      <Grid>
+        <Col>
+          <Box>
+            <Flex px={6} my={15} flexDirection="column" alignItems="center">
+              <Display color="black100" size="10" textAlign="center">
+                Choose your Gift
+              </Display>
+              <Spacer mb={5} />
+              <Sans color="black50" size="6" textAlign="center">
+                All gifts are sent via email so your recipient can redeem and customize themselves!
+                <br />
+                Gifts are one-time payments that do not renew.
+              </Sans>
+              <Spacer mb={1} />
+            </Flex>
+
+            <Box px={[2, 2, 2, 5, 5]}>
+              <Separator />
+            </Box>
+
+            <Flex my={5} flexDirection="column" alignItems="center">
+              <Flex flexDirection="row" alignItems="center">
+                <Sans color="black50" size="3" textAlign="center">
+                  Please enter your recipient's zipcode to see what plans are available for them
+                </Sans>
+                <Spacer ml={5} />
+                <TextField
+                  value={zipcode}
+                  placeholder={"Zipcode"}
+                  onChange={(e) => {
+                    setZipcode(e.target.value)
+                  }}
+                  error={!zipcode.match(ZIPCODE_REGEX)}
+                  required
+                />
+              </Flex>
+            </Flex>
+
+            <Spacer mb={2} />
           </Box>
-          <Flex flexDirection="column" width="100%">
-            {descriptionLines.map((line) => {
-              return (
-                <Flex flexDirection="row" pb={1} px={1} alignItems="center" key={line} width="100%">
-                  <Box mx={1} mr={2}>
-                    <ListCheck />
-                  </Box>
-                  <Sans color="black50" size="3">
-                    {line}
-                  </Sans>
-                </Flex>
-              )
-            })}
-          </Flex>
-          <Spacer mb={1} />
-          <TabBar
-            tabColor={currentColor}
-            spaceEvenly
-            tabs={tiers}
-            strikethroughTabs={allAccessEnabled ? [] : ["All Access"]}
-            activeTab={currentView}
-            goToPage={(page) => {
-              tracking.trackEvent({
-                actionName:
-                  page === 0 ? Schema.ActionNames.Tier0PlanTabTapped : TrackSchema.ActionNames.Tier1PlanTabTapped,
-                actionType: Schema.ActionTypes.Tap,
+        </Col>
+
+        <Col>
+          <Box px={[2, 2, 2, 5, 5]}>
+            <Separator />
+          </Box>
+
+          <Grid px={[2, 2, 2, 5, 5]} pt={[10, 0, 0, 0, 0]}>
+            <Row>
+              {plansGroupedByTier.map((group, index) => (
+                <Col md="6" xs="12" key={index}>
+                  <Flex key={index} width="100%" height="100%" px={[2, 2, 2, 7, 7]} style={{ position: "relative" }}>
+                    <Flex flexDirection="column" alignItems="center" width="100%">
+                      <Spacer pb={[0, 10, 10, 10, 10]} />
+                      <Box width="100%">
+                        <PlanTier
+                          group={group}
+                          onSelectPlan={onSelectPlan}
+                          selectedPlan={selectedPlan}
+                          allAccessEnabled={allAccessEnabled}
+                          displayText
+                        />
+                      </Box>
+                      <Spacer pb={[10, 10, 10, 10, 10]} />
+                    </Flex>
+                    <Media greaterThanOrEqual="md">
+                      {index === 0 && (
+                        <Box
+                          style={{
+                            position: "absolute",
+                            right: 0,
+                            top: 0,
+                            width: "1px",
+                            backgroundColor: color("black15"),
+                            height: "100%",
+                          }}
+                        />
+                      )}
+                    </Media>
+                  </Flex>
+                </Col>
+              ))}
+            </Row>
+          </Grid>
+
+          <Box px={[2, 2, 2, 5, 5]}>
+            <Separator />
+          </Box>
+        </Col>
+
+        <Box style={{ textAlign: "center" }} my={3}>
+          <Button
+            onClick={() => {
+              executeChargebeeCheckout({
+                planID: `${selectedPlan.planID}-gift`,
+                // TODO: read recipients email
+                email: "",
+                isGift: true,
               })
-              if (page === 1 && !allAccessEnabled) {
-                showPopUp({
-                  title: "Not available in your city yet",
-                  note: "All Access is disabled in your area due to shipping time.",
-                  buttonText: "Got it",
-                  onClose: hidePopUp,
-                })
-              } else {
-                setCurrentView(page as number)
-              }
             }}
-          />
-          <Spacer mb={2} />
-          {plans
-            ?.filter((plan) => tierToReadableText(plan.tier) === tiers?.[currentView])
-            ?.sort((a, b) => b.itemCount - a.itemCount)
-            ?.map((plan) => {
-              return (
-                <Box key={plan.id} px={2}>
-                  <PlanButton
-                    plan={plan}
-                    key={plan.id}
-                    shouldSelect={setSelectedPlan}
-                    selected={selectedPlan?.id === plan.id}
-                    selectedColor={currentColor}
-                  />
-                </Box>
-              )
-            })}
+            size="large"
+          >
+            Choose Gift
+          </Button>
         </Box>
-      </Flex>
-      <Box>
-        <Button
-          onClick={() => {
-            executeChargebeeCheckout({
-              planID: "essential-2-gift",
-              // TODO: read recipients email
-              email,
-              isGift: true,
-            })
-          }}
-          block
-        >
-          Give a Gift
-        </Button>
-      </Box>
+      </Grid>
     </Layout>
   )
 })
@@ -220,7 +192,7 @@ export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
   await apolloClient.query({
-    query: NAVIGATION_QUERY,
+    query: GET_GIFT_PAGE,
   })
 
   return {
