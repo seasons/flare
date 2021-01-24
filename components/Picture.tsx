3f63fd042bea1d0ac5e76ced9988e79112ae890a
Luc Succes <l2succes@gmail.com>
2021-01-24 13:00:00 -0500

    Update Picture component

--- components/Picture.tsx
+++ components/Picture.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { ImageProps } from "react-native"
-
-const isProd = process.env.ENVIRONMENT === "production"
+import { imageResize, ImageSize } from "utils/imageResize"
 
 export type ImagePropsWithoutSource = Omit<ImageProps, "source">
 
@@ -10,14 +9,17 @@ interface PictureProps extends ImagePropsWithoutSource {
   imgRef?: any
   onLoad?: (event: any) => void
   alt?: string
+  size?: ImageSize
 }
 
-export const Picture: React.FC<PictureProps> = ({ src, alt, imgRef, onLoad }) => {
+export const Picture: React.FC<PictureProps> = ({ src, alt, imgRef, onLoad, size, style }) => {
+  const url = !!size ? imageResize(src, size) : src
+
   return (
     <picture style={{ height: "100%" }}>
-      <source type="image/webp" srcSet={src + "&fm=webp&cs=srgb"} />
-      <source type="image/jpeg" srcSet={src + "&fm=jpg"} />
-      <img src={src + "&fm=jpg"} ref={imgRef} alt={alt} onLoad={onLoad} />
+      <source type="image/webp" srcSet={url + "&fm=webp&cs=srgb"} />
+      <source type="image/jpeg" srcSet={url + "&fm=jpg"} />
+      <img src={url + "&fm=jpg"} ref={imgRef} alt={alt} onLoad={onLoad} />
     </picture>
   )
 }
