dc25464723b12ee31b6347914fb99ca2a9710729
Kieran Gillen <kgillen@gmail.com>
2021-02-01 21:35:22 -0500

    Adds images to waitlisted and accepted views

--- components/Forms/FormConfirmation.tsx
+++ components/Forms/FormConfirmation.tsx
@@ -1,8 +1,10 @@
 import { CheckWithBackground } from "components/SVGs"
+import { space } from "helpers"
 import Link from "next/link"
 import styled from "styled-components"
+import { imageResize } from "utils/imageResize"
 
-import { Box, ExternalLink, Flex, Sans, Spacer } from "../"
+import { Box, ExternalLink, Flex, Sans, Spacer, Picture } from "../"
 import { Button } from "../Button/Button"
 import { GetTheAppButton } from "../Button/GetTheApp"
 import { FormFooter } from "./FormFooter"
@@ -16,12 +18,15 @@ export const FormConfirmation: React.FC<FormConfirmationProps> = ({ status }) =>
   let icon
   let headerText
   let bodyText
+  let imageURL
 
   if (status === "waitlisted") {
+    imageURL = require("../../public/images/waitlisted/waitlistImage.png")
     icon = <CheckWithBackground backgroundColor={"#000"} />
     headerText = "You're Waitlisted"
     bodyText = "We’ll let you know when your account is ready and you’re able to choose your plan."
   } else {
+    imageURL = require("../../public/images/welcome/WelcomeImage.png")
     icon = <CheckWithBackground />
     headerText = "Welcome to Seasons"
     bodyText = "Your membership is active and you’re ready to start reserving. Tap below to start browsing."
@@ -36,34 +41,39 @@ export const FormConfirmation: React.FC<FormConfirmationProps> = ({ status }) =>
   )
   return (
     <>
-      <Wrapper px={[2, 2, 2, 5, 5]} style={{ width: "100%" }}>
-        {icon}
-        <Spacer mb={3} />
-        <HeaderText>{headerText}</HeaderText>
-        <Spacer mb={1} />
-        <Sans size="4" color="black50" style={{ maxWidth: "800px" }}>
-          {bodyText}
-        </Sans>
-        <Spacer mb={3} />
-        <Flex flexDirection="row">
-          <Link href="/browse">
-            <Button variant="primaryBlack">
-              <Flex flexDirection="row" alignContent="center">
-                <Sans size="4">Start Browsing</Sans>
-              </Flex>
-            </Button>
-          </Link>
-          <Spacer mr={1} />
-          <GetTheAppButton />
+      <Flex flex={1} style={{ height: "100%" }} flexDirection="row" alignItems="center">
+        {!!imageURL && <ImageContainer url={imageResize(imageURL, "large")} />}
+        <Flex flexDirection="column" justifyContent="center" pl={3}>
+          {icon}
+          <Spacer mb={3} />
+          <HeaderText>{headerText}</HeaderText>
+          <Spacer mb={1} />
+          <Sans size="4" color="black50" style={{ maxWidth: "800px" }}>
+            {bodyText}
+          </Sans>
+          <Spacer mb={3} />
+          <Flex flexDirection="row">
+            <Link href="/browse">
+              <Button variant="primaryBlack">
+                <Flex flexDirection="row" alignContent="center">
+                  <Sans size="4">Start Browsing</Sans>
+                </Flex>
+              </Button>
+            </Link>
+            <Spacer mr={1} />
+            <GetTheAppButton />
+          </Flex>
         </Flex>
-      </Wrapper>
+      </Flex>
       <FormFooter footerText={footerText} disabled={false} />
     </>
   )
 }
 
-const Wrapper = styled(Box)`
-  transform: translateY(-50%);
-  top: 50%;
-  position: absolute;
+const ImageContainer = styled(Box)<{ url?: string }>`
+  width: 560px;
+  height: 100%;
+  background: url(${(p) => p.url}) no-repeat center center;
+  background-size: contain;
+  margin-right: ${space(3)}px;
 `
