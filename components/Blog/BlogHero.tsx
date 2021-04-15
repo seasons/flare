2974eb3dddcdba48b01f942d042cd423fa6b1e89
Kieran Gillen <kgillen@gmail.com>
2021-04-15 14:14:22 -0400

    Adds blog landing page

---
+++ components/Blog/BlogHero.tsx
@@ -0,0 +1,24 @@
+import { Display, Flex, ProgressiveImage, Sans, Spacer } from "components"
+import React from "react"
+
+export const BlogHero = ({ blogPost }) => {
+  const node = blogPost?.node
+  const category = node?.category
+  const name = node?.name
+  const image = node?.image
+
+  return (
+    <Flex width="100%" flexDirection="column" style={{ maxWidth: "1100px" }} px={[0, 0, 2, 2, 2]}>
+      <Spacer mb={10} />
+      <Sans color="black50" size="2" style={{ textTransform: "uppercase" }}>
+        {category}
+      </Sans>
+      <Spacer mb={0.5} />
+      <Display size="10" style={{ maxWidth: "800px" }}>
+        {name}
+      </Display>
+      <Spacer mb={5} />
+      <ProgressiveImage imageUrl={image?.url} size="hero" alt={`Image for ${name}`} aspectRatio={0.7} />
+    </Flex>
+  )
+}
