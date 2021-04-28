b182f3c6ab63f9fac154fc3b4a8f6f50238dcc08
Kieran Gillen <kgillen@gmail.com>
2021-04-28 11:03:44 -0700

    Adds desktop skeleton

--- components/Blog/BlogHero.tsx
+++ components/Blog/BlogHero.tsx
@@ -1,5 +1,7 @@
 import { Box, Display, Flex, Link, ProgressiveImage, Sans, Spacer } from "components"
 import React from "react"
+import ContentLoader from "react-content-loader"
+import styled from "styled-components"
 
 export const BlogHero = ({ blogPost }) => {
   const node = blogPost?.node
@@ -7,11 +9,11 @@ export const BlogHero = ({ blogPost }) => {
   const name = node?.name
   const image = node?.image
   const slug = node?.slug
+  const loading = !blogPost
 
-  return (
-    <Flex width="100%" flexDirection="column" px={[0, 0, 2, 2, 2]}>
-      <Spacer mb={10} />
-      <Link href="/blog/[BlogPost]" as={`/blog/${slug}`}>
+  const TextContent = () => {
+    return (
+      <>
         <Sans color="black50" size="2" style={{ textTransform: "uppercase" }}>
           {category}
         </Sans>
@@ -19,11 +21,53 @@ export const BlogHero = ({ blogPost }) => {
         <Display size="10" style={{ maxWidth: "800px" }}>
           {name}
         </Display>
+      </>
+    )
+  }
+
+  const TextLoader = () => {
+    return (
+      <>
+        <ContentLoader width="100%" height="98px" style={{ maxWidth: "100%" }}>
+          <rect x={0} y={0} width="50" height={12} />
+          <rect x={0} y={24} width="600" height={32} />
+          <rect x={0} y={68} width="200" height={32} />
+        </ContentLoader>
+      </>
+    )
+  }
+
+  const ImageLoader = () => {
+    return (
+      <ImageLoaderWrapper>
+        <ContentLoader width="100%" height="100%" style={{ position: "absolute", top: 0, left: 0 }}>
+          <rect x={0} y={0} width="100%" height="100%" />
+        </ContentLoader>
+      </ImageLoaderWrapper>
+    )
+  }
+
+  return (
+    <Flex width="100%" flexDirection="column" px={[0, 0, 2, 2, 2]}>
+      <Spacer mb={10} />
+      <Link href="/blog/[BlogPost]" as={`/blog/${slug}`}>
+        {loading ? <TextLoader /> : <TextContent />}
         <Spacer mb={5} />
-        <Box style={{ maxWidth: "1100px" }}>
-          <ProgressiveImage imageUrl={image?.url} size="hero" alt={`Image for ${name}`} aspectRatio={0.7} />
+        <Box style={{ maxWidth: "1100px", position: "relative" }}>
+          {loading ? (
+            <ImageLoader />
+          ) : (
+            <ProgressiveImage imageUrl={image?.url} size="hero" alt={`Image for ${name}`} aspectRatio={0.7} />
+          )}
         </Box>
       </Link>
     </Flex>
   )
 }
+
+const ImageLoaderWrapper = styled(Box)`
+  width: 100%;
+  height: 0;
+  padding-bottom: calc(100% * 0.7);
+  position: relative;
+`
