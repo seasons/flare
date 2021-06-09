ba98fddf7d6e1332638f656ce03e1e9b1a91b4d6
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-06-09 17:08:17 -0400

    4pm --> 4pm EST

--- mobile/Reservation/Reservation.tsx
+++ mobile/Reservation/Reservation.tsx
@@ -182,7 +182,7 @@ export const Reservation = screenTrack()((props) => {
               <Sans size="3" color="black50">
                 As a reminder, orders placed{" "}
                 <Sans size="3" color="black100" style={{ textDecorationLine: "underline", display: "inline-block" }}>
-                  after 4:00pm
+                  after 4:00pm EST
                 </Sans>{" "}
                 will be processed the following business day.
               </Sans>
