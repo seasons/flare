b281e9c49202e27a85c2d58053601fc6bb874adc
Kieran Gillen <kgillen@gmail.com>
2021-09-13 13:36:06 +0100

    Updates nav height

--- components/Nav/DesktopNav.tsx
+++ components/Nav/DesktopNav.tsx
@@ -15,7 +15,7 @@ import { NavItem } from "./NavItem"
 import { NavProps } from "./Types"
 import { GetTheAppButton } from "components/Button/GetTheApp"
 
-export const DESKTOP_NAV_HEIGHT = 92
+export const DESKTOP_NAV_HEIGHT = 72
 
 export const DesktopNav = (props: NavProps) => {
   const { links, navStyles } = props
@@ -89,7 +89,7 @@ export const DesktopNav = (props: NavProps) => {
       <Spacer ml={3} />
       <GetTheAppButton
         size="medium-x"
-        variant={navStyles?.getTheAppVariant ? navStyles.getTheAppVariant : "primaryWhiteNoBorder"}
+        variant={navStyles?.getTheAppVariant ? navStyles.getTheAppVariant : "primaryWhite"}
       />
     </>
   )
@@ -114,32 +114,30 @@ export const DesktopNav = (props: NavProps) => {
   )
 
   return (
-    <>
-      <HeaderContainer style={specialStyles} backgroundColor={backgroundColor}>
-        <MaxWidth>
-          <Box width="100%">
-            <Flex ml="auto" flexDirection="row" alignItems="center" width="100%" px={[2, 2, 2, 2, 2]}>
-              <Display color={textColor} size="7">
-                SEASONS
-              </Display>
-              <Box px={4}>
-                <SearchBar />
-              </Box>
-              <Flex ml="auto" flexDirection="row" alignItems="center">
-                {links.map(renderLink)}
-                {isLoggedIn ? renderLoggedInNavLinks() : renderLoggedOutNavLinks()}
-              </Flex>
+    <HeaderContainer style={specialStyles} backgroundColor={backgroundColor}>
+      <MaxWidth>
+        <Box width="100%">
+          <Flex ml="auto" flexDirection="row" alignItems="center" width="100%" px={[2, 2, 2, 2, 2]}>
+            <Display color={textColor} size="7">
+              SEASONS
+            </Display>
+            <Box px={4}>
+              <SearchBar />
+            </Box>
+            <Flex ml="auto" flexDirection="row" alignItems="center">
+              {links.map(renderLink)}
+              {isLoggedIn ? renderLoggedInNavLinks() : renderLoggedOutNavLinks()}
             </Flex>
-          </Box>
-        </MaxWidth>
-        <LoginModal
-          open={loginModalOpen}
-          onClose={() => {
-            toggleLoginModal(false)
-          }}
-        />
-      </HeaderContainer>
-    </>
+          </Flex>
+        </Box>
+      </MaxWidth>
+      <LoginModal
+        open={loginModalOpen}
+        onClose={() => {
+          toggleLoginModal(false)
+        }}
+      />
+    </HeaderContainer>
   )
 }
 
@@ -151,7 +149,7 @@ const HeaderContainer = styled.div<{ backgroundColor: string }>`
   left: 0;
   right: 0;
   display: flex;
-  padding-top: 16px;
+  padding-top: 8px;
   flex-direction: row;
   box-sizing: border-box;
   z-index: 100;
