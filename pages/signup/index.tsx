ea45dabaf36967bbb044238401dd2944ba1c15ce
Kieran Gillen <kgillen@gmail.com>
2021-09-23 14:28:21 +0100

    Update px on mobile

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -254,7 +254,7 @@ const SignUpPage = screenTrack(() => ({
               flexDirection="row"
               alignItems="center"
               justifyContent="center"
-              px={[2, 2, 2, 2, 2]}
+              px={[1, 1, 2, 2, 2]}
             >
               <Flex style={{ flex: 1, height: "100%", width: "100%", paddingBottom: SIGNUP_FOOTER_HEIGHT }}>
                 {CurrentStep}
