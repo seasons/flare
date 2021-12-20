11832549388028809b53cae2584d768aa373d468
Luc Succes <l2succes@gmail.com>
2021-12-20 09:47:04 -0500

    Update gift view

--- pages/gift.tsx
+++ pages/gift.tsx
@@ -1,7 +1,7 @@
 import { initializeApollo } from "lib/apollo/apollo"
 import React from "react"
 
-import { Layout } from "../components"
+import { Box, Layout } from "../components"
 import { Grid } from "../components/Grid"
 import { Schema, screenTrack } from "../utils/analytics"
 
@@ -12,13 +12,15 @@ export const GiftPage = screenTrack(() => ({
   return (
     <Layout>
       <Grid px={[2, 2, 2, 2, 2]}>
-        <div
-          className="gift-up-target"
-          data-site-id="f6a29c47-7692-4936-8c95-bfb707c2e95c"
-          data-platform="Other"
-          data-language="en-US"
-        ></div>
-        <script type="text/javascript" src="/js/giftup.js"></script>
+        <Box style={{ minHeight: "600px" }}>
+          <div
+            className="gift-up-target"
+            data-site-id="f6a29c47-7692-4936-8c95-bfb707c2e95c"
+            data-platform="Other"
+            data-language="en-US"
+          ></div>
+          <script type="text/javascript" src="/js/giftup.js"></script>
+        </Box>
       </Grid>
     </Layout>
   )
